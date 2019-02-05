5fa3e98851a9945eaf233ba3c2174fcaa7f1d4c4
abel <abelnetworksuport@gmail.com>
2019-02-05 18:08:11 +0700

    modal tambah dan modal edit

--- resources/views/pasien/type_pendaftaran.blade.php
+++ resources/views/pasien/type_pendaftaran.blade.php
@@ -35,8 +35,7 @@
 						<h2 class="card-title">Tipe Pendaftaran</h2>
 					</header>
 					<div class="card-body">
-                    <a class="mb-1 mt-1 mr-1 modal-sizes btn btn-default" href="#modalMD">Medium</a>
-                        <button type="button" class="btn btn-success"  style="margin-bottom: 10px" title="Tambah Tipe Pendaftaran !">
+                        <button type="button"  href="#modalMD" class="btn btn-success modal-sizes "  style="margin-bottom: 20px" title="Tambah Tipe Pendaftaran !">
                             Tambah
                             <i class="fa fa-plus"></i>
                         </button>
@@ -54,7 +53,7 @@
 							    <td >1</td>
 							    <td>Umum</td>
 							    <td>
-                                    <button type="button" href="" class="btn-sm btn-warning"  title="Ubah Data !" style="margin-right:5px"><i class="fa fa-edit" aria-hidden="true"></i></button>
+                                    <button type="button" class="btn-sm btn-warning modal-sizes"  title="Ubah Data !" style="margin-right:5px" href="#modalMDEdit"><i class="fa fa-edit" aria-hidden="true"></i></button>
                                     <button type="button" href="" class="btn-sm btn-danger"  title="Hapus Data !" style="margin-right:5px"><i class="fa fa-trash" aria-hidden="true"></i></button>
                                 </td>  
 						    </tr>
@@ -82,35 +81,68 @@
 		</div>
         <!-- bagian body -->
         <!-- modal tambah -->
-        <div id="modalMD" class="modal-block modal-block-md mfp-hide">
+        <div id="modalMD" class="modal-block modal-header-color modal-block-success mfp-hide">
 			<section class="card">
 				<header class="card-header">
-					<h2 class="card-title">Are you sure?</h2>
+					<h2 class="card-title">Tambah Tipe Pendaftaran</h2>
 				</header>
 				<div class="card-body">
 					<div class="modal-wrapper">
 					    <div class="modal-text">
-							<p class="mb-0">Are you sure that you want to delete this image?</p>
+                            <label class="control-label">Tipe Pendaftaran <span class="required">*</span></label>
+								
+							 <input type="text" name="fullname" class="form-control" placeholder="Tipe Pendaftaran" required/>
+							    
 						</div>
 					</div>
 				    </div>
-						<footer class="card-footer">
-							<div class="row">
-								<div class="col-md-12 text-right">
-									<button class="btn btn-primary modal-confirm">Confirm</button>
-									<button class="btn btn-default modal-dismiss">Cancel</button>
-								</div>
-							</div>
-						</footer>
+				<footer class="card-footer">
+					<div class="row">
+					    <div class="col-md-12 text-right">
+                            <button class="btn btn-default modal-dismiss">Batal</button>
+							<button type="submit" class="btn btn-success modal-confirm">Simpan</button>
+						</div>
+					</div>
+				</footer>
 			</section>
 		</div>
         <!-- modal tambah -->
+        <!-- modal Edit -->
+        <div id="modalMDEdit" class="modal-block modal-header-color modal-block-success mfp-hide">
+			<section class="card">
+				<header class="card-header">
+					<h2 class="card-title">Ubah Tipe Pendaftaran</h2>
+				</header>
+				<div class="card-body">
+					<div class="modal-wrapper">
+					    <div class="modal-text">
+                            <label class="control-label">Tipe Pendaftaran <span class="required">*</span></label>
+								
+							 <input type="text" name="fullname" class="form-control" placeholder="Tipe Pendaftaran" required/>
+							    
+						</div>
+					</div>
+				    </div>
+				<footer class="card-footer">
+					<div class="row">
+					    <div class="col-md-12 text-right">
+                            <button class="btn btn-default modal-dismiss">Batal</button>
+							<button type="submit" class="btn btn-success modal-confirm">Simpan</button>
+						</div>
+					</div>
+				</footer>
+			</section>
+		</div>
+        <!-- modal Edit-->
 </section>
 @endsection
 @section('css')
-  
+<link rel="stylesheet" href="{{ asset('assets/vendor/select2/css/select2.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/vendor/select2-bootstrap-theme/select2-bootstrap.min.css') }}" />
+
 @stop
 @section('script')
-   <script src="js/examples/examples.modals.js"></script>
+<script src="{{ asset('assets/js/examples/examples.modals.js') }}"></script>
+  
 
 @stop
