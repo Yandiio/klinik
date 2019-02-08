6b6510d280b64b651d25817f11b25e5e8109fa35
cahyadihelmay <cahyadihelmay@gmail.com>
2019-02-08 17:01:21 +0700

    tambah view apotik

--- resources/views/apotik/tipeobat/list.blade.php
+++ resources/views/apotik/tipeobat/list.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.admin')
 
-@yield('title', 'List Apotik')
+@yield('title', 'List Tipe Obat')
 
 @section('content')
 <section role="main" class="content-body">
@@ -15,7 +15,7 @@
                     </a>
                 </li>
                 <li><span>Apotik</span></li>
-                <li><span>List Apotik</span></li>
+                <li><span> List Tipe Obat </span></li>
             </ol>
     
             <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fas fa-chevron-left"></i></a>
@@ -32,18 +32,18 @@
 							<a href="#" class="card-action card-action-dismiss" data-card-dismiss></a>
 						</div>
 						
-						<h2 class="card-title">List Apotik</h2>
+						<h2 class="card-title">List Tipe Obat</h2>
 					</header>
 					<div class="card-body">
                         <button type="button"  href="#modalMD" class="btn btn-success modal-sizes "  style="margin-bottom: 20px" title="Tambah Tipe Pendaftaran !">
-                            Tambah
+                            Tambah Tipe Obat
                             <i class="fa fa-plus"></i>
                         </button>
-						<table class="table table-responsive-lg table-bordered table-striped table-sm mb-0">
+                        <table class="table table-responsive-lg table-bordered table-striped table-sm mb-0">
 						<thead>
 						    <tr>
 							    <th width="5%">No</th>
-								<th>Tipe Pendaftaran</th>
+                                <th>Nama Tipe Obat</th>
 								<th width="15%">Aksi</th>
 								
 							</tr>
@@ -51,7 +51,8 @@
 						<tbody>
                             <tr>
 							    <td >1</td>
-							    <td>Umum</td>
+                                <td>Serbuk</td>
+                                
 							    <td>
                                     <button type="button" class="btn-sm btn-warning modal-sizes"  title="Ubah Data !" style="margin-right:5px" href="#modalMDEdit"><i class="fa fa-edit" aria-hidden="true"></i></button>
                                     <button type="button" href="" class="btn-sm btn-danger"  title="Hapus Data !" style="margin-right:5px"><i class="fa fa-trash" aria-hidden="true"></i></button>
@@ -59,17 +60,28 @@
 						    </tr>
                             <tr>
 							    <td >2</td>
-							    <td>Pribadi</td>
+                                <td>Sirup</td>
+                               
 							    <td>
-                                    <button type="button" href="" class="btn-sm btn-warning"  title="Ubah Data !" style="margin-right:5px"><i class="fa fa-edit" aria-hidden="true"></i></button>
+                                    <button type="button" class="btn-sm btn-warning modal-sizes"  title="Ubah Data !" style="margin-right:5px" href="#modalMDEdit"><i class="fa fa-edit" aria-hidden="true"></i></button>
                                     <button type="button" href="" class="btn-sm btn-danger"  title="Hapus Data !" style="margin-right:5px"><i class="fa fa-trash" aria-hidden="true"></i></button>
                                 </td>  
 						    </tr>
                             <tr>
-							    <td >3</td>
-							    <td>Bpjs</td>
+                                <td >3</td>
+                                <td>Tablet</td>
+							    
 							    <td>
-                                    <button type="button" href="" class="btn-sm btn-warning"  title="Ubah Data !" style="margin-right:5px"><i class="fa fa-edit" aria-hidden="true"></i></button>
+                                    <button type="button" class="btn-sm btn-warning modal-sizes"  title="Ubah Data !" style="margin-right:5px" href="#modalMDEdit"><i class="fa fa-edit" aria-hidden="true"></i></button>
+                                    <button type="button" href="" class="btn-sm btn-danger"  title="Hapus Data !" style="margin-right:5px"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                                </td>  
+                            </tr>
+                            <tr>
+                                <td >4</td>
+                                <td>Kapsul</td>
+							    
+							    <td>
+                                    <button type="button" class="btn-sm btn-warning modal-sizes"  title="Ubah Data !" style="margin-right:5px" href="#modalMDEdit"><i class="fa fa-edit" aria-hidden="true"></i></button>
                                     <button type="button" href="" class="btn-sm btn-danger"  title="Hapus Data !" style="margin-right:5px"><i class="fa fa-trash" aria-hidden="true"></i></button>
                                 </td>  
 						    </tr>
@@ -84,16 +96,14 @@
         <div id="modalMD" class="modal-block modal-header-color modal-block-success mfp-hide">
 			<section class="card">
 				<header class="card-header">
-					<h2 class="card-title">Tambah Tipe Pendaftaran</h2>
+					<h2 class="card-title">Tambah Tipe Obat</h2>
 				</header>
 				<div class="card-body">
 					<div class="modal-wrapper">
 					    <div class="modal-text">
-                            <label class="control-label">Tipe Pendaftaran <span class="required">*</span></label>
-								
-							 <input type="text" name="fullname" class="form-control" placeholder="Tipe Pendaftaran" required/>
-							    
-						</div>
+                            <label class="control-label">Tipe Obat<span class="required">*</span></label>
+							 <input type="text" name="fullname" class="form-control" placeholder="Tipe Poli" required/>
+                        </div>
 					</div>
 				    </div>
 				<footer class="card-footer">
@@ -111,18 +121,18 @@
         <div id="modalMDEdit" class="modal-block modal-header-color modal-block-success mfp-hide">
 			<section class="card">
 				<header class="card-header">
-					<h2 class="card-title">Ubah Tipe Pendaftaran</h2>
+					<h2 class="card-title">Ubah Tipe Obat</h2>
 				</header>
 				<div class="card-body">
 					<div class="modal-wrapper">
 					    <div class="modal-text">
-                            <label class="control-label">Tipe Pendaftaran <span class="required">*</span></label>
+                            <label class="control-label">Tipe Obat <span class="required">*</span></label>
 								
-							 <input type="text" name="fullname" class="form-control" placeholder="Tipe Pendaftaran" required/>
+							 <input type="text" name="fullname" value="Serbuk" class="form-control" required/>
 							    
 						</div>
 					</div>
-				    </div>
+				</div>
 				<footer class="card-footer">
 					<div class="row">
 					    <div class="col-md-12 text-right">
