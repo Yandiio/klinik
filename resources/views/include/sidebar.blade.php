158d511149d1d809f3f97ea896a884e0330df0c9
cahyadihelmay <cahyadihelmay@gmail.com>
2019-02-09 11:06:07 +0700

    rubah menu racikan jadi resep

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -122,7 +122,7 @@
                             </li>
                             <li>
                                 <a class="nav-link" href="forms-basic.html">
-                                    Racikan Obat
+                                    Resep
                                 </a>
                             </li>
                             <li>
